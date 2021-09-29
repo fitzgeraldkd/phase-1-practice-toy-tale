05bb5e5efbc08cb20b53002658a0d6998dc56ee9
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-09-29 09:54:59 -0400

    commit tree dfbbdec05d197522302755cd62540cf698a90367

--- src/index.js
+++ src/index.js
@@ -12,4 +12,85 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  document.querySelector('form.add-toy-form').addEventListener('submit', submitToy)
+  const renderToys = (results) => results.forEach(renderToy);
+  sendRequest('http://localhost:3000/toys', renderToys);
 });
+
+function renderToy(toy) {
+  const collection = document.getElementById('toy-collection');
+  const toyCard = document.createElement('div');
+  toyCard.className = 'card';
+  toyCard.id = `card-${toy.id}`
+
+  const toyName = document.createElement('h2');
+  toyName.textContent = toy.name;
+  const toyImg = document.createElement('img');
+  toyImg.src = toy.image;
+  toyImg.className='toy-avatar';
+  const toyLikes = document.createElement('p');
+  toyLikes.textContent = `${toy.likes} Likes`;
+  const toyLikeBttn = document.createElement('button');
+  toyLikeBttn.textContent = 'Like <3';
+  toyLikeBttn.id = toy.id;
+  toyLikeBttn.className = 'like-btn';
+  toyLikeBttn.addEventListener('click', addLike);
+
+  toyCard.append(toyName, toyImg, toyLikes, toyLikeBttn);
+  collection.append(toyCard);
+
+}
+
+function submitToy(e) {
+  e.preventDefault();
+  const form = e.target;
+
+  const body = {
+    'name': form.querySelector('input[name="name"]').value,
+    'image': form.querySelector('input[name="image"]').value,
+    'likes': 0
+  };
+
+  const url = 'http://localhost:3000/toys';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(body)
+  }
+  sendRequest(url, renderToy, options);
+
+  form.reset();
+}
+
+function addLike(e) {
+  const toyId = e.target.id;
+  const url = `http://localhost:3000/toys/${toyId}`;
+  // get likes, add one, patch likes
+  const cbPatchLikes = (options) => sendRequest(url, updateLikes, options);
+  const cbModifyLikes = (toy) => {
+    toy.likes++;
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(toy)
+    }
+    cbPatchLikes(options);
+  };
+  sendRequest(url, cbModifyLikes);
+}
+
+function updateLikes(toy) {
+  const card = document.getElementById(`card-${toy.id}`);
+  const likes = card.querySelector('p');
+  likes.textContent = `${toy.likes} Likes`
+}
+
+function sendRequest(url, callback, options={}) {
+  fetch(url, options).then(resp => resp.json()).then(callback);
+}
